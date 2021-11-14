6546b6059626bb1b92f132eae2c93c757b293bbb
testernumba1 <warmaster007228@gmail.com>
2021-11-14 13:21:35 +0200

    V27

--- applicationstart.sh
+++ applicationstart.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 cd /var/www/html
-sudo systemctl stop httpd.service
-rm /var/www/html/*
-mv /home/index.html /var/www/html
-rm /
-sudo systemctl start httpd.service
+systemctl stop httpd.service
